e67e1c88b17b561e41164c653484deed7dd2d326
Nastsukii <lacb.lzn.luiz@gmail.com>
2023-11-03 17:49:04 -0300

    adição de um valor maior que o outro

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ function generateNumber(){
     let min = Math.ceil(document.querySelector('.input-min').value)
     let max = Math.floor(document.querySelector('.input-max').value)
     
-    let result = Math.floor(Math.random() * (max - min + 1)) + min;
+   
+    
+    if(min > max){
+alert('O valor minimo tem que ser maior que o valor maximo')
+    }else{
+        let result = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert(result)
+    }
 
-    alert(result)
+
+    
+
+    
 }
